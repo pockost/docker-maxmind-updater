3dc75d5bb3c1594b23e3b4cffeca7f77413f187c
Romain THERRAT <romain42@gmail.com>
2019-06-11 17:29:16 +0200

    feat(startup): Download on first run

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 echo "Configuring cron"
 echo "${CRONTAB_FREQUENCY} /bin/sh /update-maxmind.sh" > /etc/crontabs/root
 
+echo "First download"
+/bin/sh /update-maxmind.sh
+
 echo "Starting Cron..."
 exec "$@"
